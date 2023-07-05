2070365537400a4ab027d04a185925aa005a4d97
一纸忘忧 <i@ikxin.com>
2023-07-05 15:27:09 +0800

    upload: uptime-status

---
+++ uptime-status/config.js
@@ -0,0 +1,33 @@
+window.Config = {
+
+  // 显示标题
+  SiteName: 'Public Status',
+
+  // UptimeRobot Api Keys
+  // 支持 Monitor-Specific 和 Read-Only
+  ApiKeys: [
+    'ur886564-129091d7ca15b1fbac4b2e28',
+  ],
+
+  // 日志天数
+  CountDays: 90,
+
+  // 是否显示检测站点的链接
+  ShowLink: true,
+
+  // 导航栏菜单
+  Navi: [
+    {
+      text: 'Homepage',
+      url: 'https://status.org.cn/'
+    },
+    {
+      text: 'GitHub',
+      url: 'https://github.com/yb/uptime-status'
+    },
+    {
+      text: 'Blog',
+      url: 'https://abo.xyz/'
+    },
+  ],
+};
